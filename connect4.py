5728765b68b1a4c839fa772527be2d833d070c4f
Yisus <yisusmurci@gmail.com>
2024-01-08 15:04:15 -0500

    Conecta 4 para dos Por el momento el juego acaba al conectar en horizontal o vertical

--- connect4.py
+++ connect4.py
@@ -1,30 +1,65 @@
-startedBoard= [[0 for i in range(0, 7)] for i in range(0, 7)]
+startedBoard= [0 for i in range(0, 49)]
 
-#El array de tablero maneja los valores 0 para vacio, y 1 y -1 para los jugadores o el programa
-#The array board manega the values 0 for empty, and 1 and -1 for the players or the software
 def seeBoard(board):
-    print([str(j) for  j in range(1, 8)])
-    lines= []
     for i in range(0, 7):
         line= []
-        for j in range (0, 7):
-            if board[j][i]== -1:
+        for j in range(0, 7):
+            if board[(6-i)*7+j]== 1:
+               line.append("o")
+            elif board[(6-i)*7+j]== -1:
                 line.append("x")
-            elif board[j][i]== 1:
-                line.append("o")
             else:
                 line.append(" ")
-        lines.append(line)
-    lines.reverse()
-    for line in lines:
         print(line)
 
-def player(board, num= -1):
-    fill= input("Select the column")
-    if fill in "1234567":
-        fill= int(fill)-1
+def player(board, num=-1):
+    column= input("Select a column")
+    if column in "1234567":
+        column= int(column)-1
         for i in range(0, 7):
-            if board[fill][i]== 0:
-                board[fill][i]= num
+            if board[column+7*i] == 0:
+                board[column+7*i]= num
                 return board
-seeBoard(player(startedBoard))
+
+def tie(board):
+    if 0 in board:
+        return False
+    else:
+        return True
+
+def win(board):
+    for i in range (0, 7):
+        count= 0
+        #vertical
+        if board[21+i]!= 0:
+            for j in range(0, 7):
+                if board[j*7+i]== board[21+i]:
+                    count+=1
+                    if count==3:
+                        return True
+                else:
+                    count= 0
+        for j in range(0, 7):
+            #horizontal
+            if board[7*i+3]== 0:
+                break
+            if  j==3:
+                continue
+            if board[i*7+j]== board[i*7+3]:
+                count+=1
+                if count==3:
+                    return True
+            else:
+                count= 0
+    return False
+
+board= startedBoard[:]
+seeBoard(board)
+turn= 1
+while not tie(board):
+    board= player(board, turn)
+    turn*=-1
+    seeBoard(board)
+    if win(board):
+        print("game over")
+        break
