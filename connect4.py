be9c4b7455fc1b16dbc8e494ac5c9dc3c979499d
Yisus <yisusmurci@gmail.com>
2024-01-22 19:34:15 -0500

    Algoritmo de evaluación -Este algorimto siguiere los movimientos para hacer una línea. -El algoritmo no tiene en cuanta los movimienotos del rival.

--- connect4.py
+++ connect4.py
@@ -1,4 +1,124 @@
 startedBoard= [0 for i in range(0, 49)]
+
+def evaluateBoard(board, player= 1):
+    #Contar lineas posibles
+    #Count possible lines
+    count2= 0
+    count3= 0
+    count4= 0
+    for i in range (0, 7):
+        #vertical
+        if board[21+i]!= 0:
+            count= 0
+            num= 21+i
+            while num>= 0 and board[num]== player:
+                num-= 7
+            num+=7
+            while board[num]== player:
+                num+= 7
+                count+= 1
+            match count:
+                    case 1:
+                        break
+                    case 2:
+                        count2+= 1
+                    case 3:
+                        count3+=1
+                    case _:
+                        count4+= 1
+        #horizontal
+        if board[i*7+3]!= 0:
+            count= 0
+            num= i*7+3
+            while board[num]== player and num>= i*7:
+                num-= 1
+            num+=1
+            while board[num]== player:
+                count+= 1
+                num+=1
+            match count:
+                    case 1:
+                        break
+                    case 2:
+                        count2+= 1
+                    case 3:
+                        count3+=1
+                    case _:
+                        count4+= 1
+    #diagonal
+    diagonalPoints= [
+        [21, 3],
+        [21, 45],
+        [27, 3],
+        [27, 45],
+        [22, 10],
+        [22, 38],
+        [26, 10],
+        [26, 38],
+        [23, 17],
+        [23, 31],
+        [25, 17],
+        [25, 31],
+        [24, 24]
+    ]
+    potentialDiagonal= False
+    for points in diagonalPoints:
+        if board[points[0]]==0:
+            continue
+        if board[points[0]]== board[points[1]]:
+            player= board[points[0]]
+            potentialDiagonal= True
+        if potentialDiagonal:
+            pointsDistances= [8, 6]
+            for disntance in pointsDistances:
+                count= 0
+                i= points[0]
+                while board[i]== player and i>= 0:
+                    i-=disntance
+                #Restaurar el inicio de la conexión
+                #Restore the start of the connection
+                i+= disntance
+                while i <49:
+                    if board[i]== player:
+                        count+= 1
+                    i+= disntance
+                match count:
+                    case 1:
+                        break
+                    case 2:
+                        count2+= 1
+                    case 3:
+                        count3+=1
+                    case _:
+                        count4+= 1
+    return count2*2+ count3*9+ count4*100
+
+def sortMovements(x):
+    return x[0]
+
+def minMax(board, turn, count= 0):
+    global move
+    if win(board) or count== 6:
+        return evaluateBoard(board, 1)- evaluateBoard(board, turn*-1)
+    movements= []
+    for i in range(0, 7):
+        auxBoard= board[:]
+        for j in range(0, 7):
+            if auxBoard[i+7*j]!= 0:
+                continue
+            auxBoard[i]= turn
+            puntuation= minMax(auxBoard, turn*-1, count+1)
+            movements.append([puntuation, i])
+            break
+    if turn== 1:
+        movements.sort(key=sortMovements, reverse= True)
+        movement= movements[0]
+        move= movement[1]
+        return movement[0]
+    else:
+        movements.sort(key=sortMovements)
+        movement= movements[0]
+        return movement[0]
 def seeBoard(board):
     for i in range(0, 7):
         line= []
@@ -44,7 +164,7 @@ def win(board):
                 num+= 7
                 count+= 1
             if count== 4:
-                return True
+                return player
         #horizontal
         if board[i*7+3]!= 0:
             count= 0
@@ -57,7 +177,7 @@ def win(board):
                 count+= 1
                 num+=1
             if count== 4:
-                return True
+                return player
     #diagonal
     diagonalPoints= [
         [21, 3],
@@ -96,7 +216,7 @@ def win(board):
                     if board[i]== player:
                         count+= 1
                         if count==4:
-                            return True
+                            return player
                     else:
                         count= 0
                         break
@@ -113,6 +233,9 @@ def play():
             board= player(board, turn)
             turn*=-1
             seeBoard(board)
+            if turn== 1:
+                minMax(board, turn)
+                print(move+1)
             if win(board):
                 print("game over")
                 break
