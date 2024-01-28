f452190d119a4a8edd5cd49aadc66f2af2b6ffb2
Yisus <yisusmurci@gmail.com>
2024-01-28 16:30:40 -0500

    Organización de código

--- connect4.py
+++ connect4.py
@@ -11,6 +11,113 @@ def mark(board, turn, column):
                 break
     return newBoard
 
+def seeBoard(board):
+    for i in range(0, 7):
+        line= []
+        for j in range(0, 7):
+            if board[(6-i)*7+j]== 1:
+               line.append("o")
+            elif board[(6-i)*7+j]== -1:
+                line.append("x")
+            else:
+                line.append(" ")
+        print(line)
+    nums= [str(i) for i in range(1, 8)]
+    print(nums)
+
+def player(board, turn=-1):
+    column= input("Select a column")
+    if column in "1234567":
+        column= int(column)-1
+    marking= mark(board, turn, column)
+    if marking== board:
+        print("Something is wrong try again")
+        return player(board, turn)
+    else:
+        board= marking
+        return board
+
+def tie(board):
+    if 0 in board:
+        return False
+    else:
+        return True
+
+def win(board):
+    for i in range (0, 7):
+        #vertical
+        if board[21+i]!= 0:
+            count= 0
+            player= board[21+i]
+            num= 21+i
+            while num>= 0 and board[num]== player:
+                num-= 7
+            num+=7
+            while num< 49 and board[num]== player:
+                num+= 7
+                count+= 1
+            if count>= 4:
+                return player
+        #horizontal
+        if board[i*7+3]!= 0:
+            count= 0
+            player= board[7*i+3]
+            num= i*7+3
+            while board[num]== player and num>= i*7:
+                num-= 1
+            num+=1
+            while board[num]== player:
+                count+= 1
+                num+=1
+            if count>=4:
+                return player
+    #diagonal
+    diagonalPoints= [
+        [21, 3],
+        [21, 45],
+        [27, 3],
+        [27, 45],
+        [22, 10],
+        [22, 38],
+        [26, 10],
+        [26, 38],
+        [23, 17],
+        [23, 31],
+        [25, 17],
+        [25, 31],
+        [24, 24]
+    ]
+    player= 0
+    potentialDiagonal= False
+    for points in diagonalPoints:
+        if board[points[0]]==0:
+            continue
+        if board[points[0]]== board[points[1]]:
+            player= board[points[0]]
+            potentialDiagonal= True
+        if potentialDiagonal:
+            pointsDistances= [8, 6]
+            for disntance in pointsDistances:
+                count= 0
+                i= points[0]
+                while board[i]== player and i>= 0:
+                    i-=disntance
+                #Restaurar el inicio de la conexión
+                #Restore the start of the connection
+                i+= disntance
+                while i <49:
+                    if board[i]== player:
+                        count+= 1
+                        if count>=4:
+                            return player
+                    else:
+                        count= 0
+                        break
+                    i+= disntance
+    return False
+
+#Funciones del ordenador
+#Machine functions
 def evaluateBoard(board, player= 1):
     #Contar lineas posibles
     #Count possible lines
@@ -143,111 +250,6 @@ def minMax(board, turn, count= 0):
         movements.sort(key=sortMovements)
         movement= movements[0]
         return movement[0]
-
-def seeBoard(board):
-    for i in range(0, 7):
-        line= []
-        for j in range(0, 7):
-            if board[(6-i)*7+j]== 1:
-               line.append("o")
-            elif board[(6-i)*7+j]== -1:
-                line.append("x")
-            else:
-                line.append(" ")
-        print(line)
-    nums= [str(i) for i in range(1, 8)]
-    print(nums)
-
-def player(board, turn=-1):
-    column= input("Select a column")
-    if column in "1234567":
-        column= int(column)-1
-    marking= mark(board, turn, column)
-    if marking== board:
-        print("Something is wrong try again")
-        return player(board, turn)
-    else:
-        board= marking
-        return board
-
-def tie(board):
-    if 0 in board:
-        return False
-    else:
-        return True
-
-def win(board):
-    for i in range (0, 7):
-        #vertical
-        if board[21+i]!= 0:
-            count= 0
-            player= board[21+i]
-            num= 21+i
-            while num>= 0 and board[num]== player:
-                num-= 7
-            num+=7
-            while num< 49 and board[num]== player:
-                num+= 7
-                count+= 1
-            if count>= 4:
-                return player
-        #horizontal
-        if board[i*7+3]!= 0:
-            count= 0
-            player= board[7*i+3]
-            num= i*7+3
-            while board[num]== player and num>= i*7:
-                num-= 1
-            num+=1
-            while board[num]== player:
-                count+= 1
-                num+=1
-            if count>=4:
-                return player
-    #diagonal
-    diagonalPoints= [
-        [21, 3],
-        [21, 45],
-        [27, 3],
-        [27, 45],
-        [22, 10],
-        [22, 38],
-        [26, 10],
-        [26, 38],
-        [23, 17],
-        [23, 31],
-        [25, 17],
-        [25, 31],
-        [24, 24]
-    ]
-    player= 0
-    potentialDiagonal= False
-    for points in diagonalPoints:
-        if board[points[0]]==0:
-            continue
-        if board[points[0]]== board[points[1]]:
-            player= board[points[0]]
-            potentialDiagonal= True
-        if potentialDiagonal:
-            pointsDistances= [8, 6]
-            for disntance in pointsDistances:
-                count= 0
-                i= points[0]
-                while board[i]== player and i>= 0:
-                    i-=disntance
-                #Restaurar el inicio de la conexión
-                #Restore the start of the connection
-                i+= disntance
-                while i <49:
-                    if board[i]== player:
-                        count+= 1
-                        if count>=4:
-                            return player
-                    else:
-                        count= 0
-                        break
-                    i+= disntance
-    return False
 
 def play():
     loop= True
